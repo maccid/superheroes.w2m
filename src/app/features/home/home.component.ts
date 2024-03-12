1d77108af56ddab9fa2191c10ab2b8d46c5ad35f
Miguel Ángel Cid García <mac6656@gmail.com>
2024-03-12 18:19:59 +0100

    Features: - Pagina Inicio, Contiene documentación de los pasos seguidos

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
+import {MatExpansionModule} from '@angular/material/expansion';
+
 @Component({
   standalone: true,
+  imports: [MatExpansionModule],
   templateUrl: './home.component.html',
   styleUrls: ['home.component.scss']
 })
